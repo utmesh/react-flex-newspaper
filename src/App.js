d295607212babf26e406081cec636000e2d536a1
Umesh Thapa <utmesh5@gmail.com>
2020-06-09 11:13:01 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React from 'react';
 import './Styles/App.css';
 import NewsPaper from './newsPaper';
 
-const info1 =['Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic ad in quas alias non! Expedita similique et dolore illum doloremque vel accusantium ut eos tempora sequi, doloribus vitae mollitia praesentium ex ullam? Dolorem labore fugiat neque nostrum porro vero fugit.',
-<br/> ,'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam corrupti, eius odio neque incidunt esse fuga veritatis ipsum, eligendi mollitia, porro quasi provident nisi sit! Doloribus, eligendi nemo, ad laudantium cupiditate aspernatur eveniet asperiores modi praesentium voluptas eos sunt odio.'];
+const info1 =`Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+Hic ad in quas alias non! Expedita similique et dolore illum doloremque
+vel accusantium ut eos tempora sequi, doloribus vitae mollitia praesentium 
+ex ullam? Dolorem labore fugiat neque nostrum porro vero fugit.
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+Aliquam corrupti, eius odio neque incidunt esse fuga veritatis ipsum, 
+eligendi mollitia, porro quasi provident nisi sit! Doloribus, eligendi nemo, 
+ad laudantium cupiditate aspernatur eveniet asperiores modi praesentium voluptas eos sunt odio.`
 
 const imageLink = 'http://placecorgi.com/500/250';
 
-const text = ['Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, dolores? Illo dolores, rerum iste aut porro doloribus fugit itaque voluptas.',
-<br/>,'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium incidunt ullam ea magni ipsam perferendis ratione pariatur enim repellendus quia?'];
+const text = `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+Quos, dolores? Illo dolores, rerum iste aut porro doloribus fugit itaque voluptas.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium incidunt 
+ullam ea magni ipsam perferendis ratione pariatur enim repellendus quia?`
 
 function App() {
   return (
